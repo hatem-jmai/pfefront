05150483acf8f46d516dda602b1094b448239522
hatem-jmai <57534253+hatem-jmai@users.noreply.github.com>
2020-03-09 13:34:52 +0100

    first commit

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,19 +7,18 @@ import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { AreaComponent } from './widgets/area/area.component';
+
 import { HighchartsChartModule } from 'highcharts-angular';
-import { CardComponent } from './widgets/card/card.component';
-import { PieComponent } from './widgets/pie/pie.component';
+
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
     SidebarComponent,
-    AreaComponent,
-    CardComponent,
-    PieComponent
+    
+    
+    
   ],
   imports: [
     CommonModule,
@@ -37,9 +36,8 @@ import { PieComponent } from './widgets/pie/pie.component';
     HeaderComponent,
     FooterComponent,
     SidebarComponent,
-    AreaComponent,
-    CardComponent,
-    PieComponent
+   
+    
   ]
 })
 export class SharedModule { }
